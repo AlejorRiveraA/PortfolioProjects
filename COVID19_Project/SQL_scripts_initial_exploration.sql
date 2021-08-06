d9e1a7fca2adb01514d9c5b91ff70583f7ce7c4b
Alejandro Rivera Alvarez <alejorivera.a@gmail.com>
2021-08-07 08:51:27 +1000

    Add files via upload
    Changed the name of the DB to PortfolioProject

--- COVID19_Project/SQL_scripts_initial_exploration.sql
+++ COVID19_Project/SQL_scripts_initial_exploration.sql
@@ -1,5 +1,5 @@
 SELECT *
-FROM PortfolioProjecy.dbo.covid_deaths
+FROM PortfolioProject.dbo.covid_deaths
 ORDER BY 3,4;
 
 --SELECT *
@@ -9,30 +9,30 @@ ORDER BY 3,4;
 -- Looking at Total Deaths vs Total Cases
 -- Shows the likelihood in time of dying if you contract COVID-19 in your country
 SELECT location, date, total_cases, total_deaths, ROUND((total_deaths/total_cases)*100, 2) AS DeathPercentage
-FROM PortfolioProjecy.dbo.covid_deaths
+FROM PortfolioProject.dbo.covid_deaths
 WHERE LOWER(location) = 'colombia'
 ORDER BY 1,2 DESC;
 
 SELECT location, date, total_cases, total_deaths, ROUND((total_deaths/total_cases)*100, 2) AS DeathPercentage
-FROM PortfolioProjecy.dbo.covid_deaths
+FROM PortfolioProject.dbo.covid_deaths
 WHERE LOWER(location) LIKE '%states%'
 ORDER BY 1,2 DESC;
 
 -- Total cases vs Population
 SELECT location, date, population, total_cases, total_cases_per_million, ROUND((total_cases/population)*1000000, 2) AS PopulationPercentage
-FROM PortfolioProjecy.dbo.covid_deaths
+FROM PortfolioProject.dbo.covid_deaths
 WHERE LOWER(location) LIKE '%states%'
 ORDER BY 1,2 DESC;
 
 -- Total cases vs Population
 SELECT location, population, COUNT(*) AS numberObs
-FROM PortfolioProjecy.dbo.covid_deaths
+FROM PortfolioProject.dbo.covid_deaths
 WHERE LOWER(location) LIKE '%states%'
 GROUP BY location, population;
 
 -- Looking at countries with highest Infection Rate compared to population
 SELECT location, population, MAX(total_cases) AS highestInfectionCount, ROUND(MAX((total_cases/population))*100, 2) AS PercentPopulationInfected
-FROM PortfolioProjecy.dbo.covid_deaths
+FROM PortfolioProject.dbo.covid_deaths
 --WHERE LOWER(location) LIKE '%states%'
 GROUP BY location, population
 ORDER BY PercentPopulationInfected DESC;
@@ -41,7 +41,7 @@ ORDER BY PercentPopulationInfected DESC;
 -- The column total_deaths is nvarchar type. Casting to integer is needed here
 -- the location contains continents
 SELECT location, MAX(CAST(total_deaths AS INT)) AS HighestDeath
-FROM PortfolioProjecy.dbo.covid_deaths
+FROM PortfolioProject.dbo.covid_deaths
 WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY HighestDeath DESC;
@@ -50,7 +50,7 @@ ORDER BY HighestDeath DESC;
 SELECT t1.continent, SUM (t1.HighestDeath) AS Total_deaths
 FROM (
 	SELECT continent, location, MAX(CAST(total_deaths AS INT)) AS HighestDeath
-	FROM PortfolioProjecy.dbo.covid_deaths
+	FROM PortfolioProject.dbo.covid_deaths
 	WHERE continent IS NOT NULL
 	GROUP BY continent, location
 	) t1
@@ -59,15 +59,15 @@ ORDER BY Total_deaths DESC
 ;
 
 SELECT location, MAX(CAST(total_deaths AS INT)) AS HighestDeath
-FROM PortfolioProjecy.dbo.covid_deaths
+FROM PortfolioProject.dbo.covid_deaths
 WHERE continent IS NULL
 GROUP BY location
 ORDER BY HighestDeath DESC;
 
 -- For each continent what country has the highest total of deaths?
 SELECT Td.continent, Td.location, CAST(Td.total_deaths AS INT) AS total_deaths_int, Td.total_cases
-FROM PortfolioProjecy.dbo.covid_deaths Td, (SELECT continent, MAX(CAST(total_deaths AS INT)) AS HighestDeath
-	FROM PortfolioProjecy.dbo.covid_deaths
+FROM PortfolioProject.dbo.covid_deaths Td, (SELECT continent, MAX(CAST(total_deaths AS INT)) AS HighestDeath
+	FROM PortfolioProject.dbo.covid_deaths
 	WHERE continent IS NOT NULL --AND continent= 'North America'
 	GROUP BY continent) T2
 WHERE Td.continent = T2.continent AND td.total_deaths = T2.HighestDeath
@@ -78,8 +78,8 @@ ORDER BY total_deaths_int DESC
 -- Looking at total populaiton vs Vaccinations
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
 	SUM(CAST(vac.new_vaccinations AS INT)) OVER (Partition by dea.location ORDER BY dea.location, dea.date)
-FROM PortfolioProjecy.dbo.covid_deaths dea
-JOIN PortfolioProjecy.dbo.covid_vaccinations vac
+FROM PortfolioProject.dbo.covid_deaths dea
+JOIN PortfolioProject.dbo.covid_vaccinations vac
 ON dea.location = vac.location
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
